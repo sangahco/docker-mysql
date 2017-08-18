960a3d3fb39602c1a8ac6b8ab0d51634869e0612
Emanuele Disco <emawind84@gmail.com>
2017-08-18 16:39:47 +0900

    update

--- docker-auto.sh
+++ docker-auto.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#export CERTBOT_CERTS_PATH=/etc/letsencrypt
-#export CERTBOT_HOST=dev.sangah.com
-#export CERTBOT_EMAIL=pmis@sangah.com
-#export CERTBOT_WEBROOT=/var/www
-
 set -e
 
 ###############################################
